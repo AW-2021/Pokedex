0628a07440661225bcb828f4f2286a8ad7c86f5e
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-07-14 17:09:03 +0500

    Removed old pokedex container

--- src/App.tsx
+++ src/App.tsx
@@ -17,17 +17,8 @@ const App = () => {
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
   const [showText, setShowText] = useState<boolean>(false);
 
-  const tableDataStyle: string = "border-3 border-white p-[0.6rem]";
+  const tableDataStyle: string = "border-2 border-gray-900 text-[min(2vw,1rem)] p-1 md:p-2 lg:p-3";
   const baseTypeClass: string = "w-[min(10vw,4.5rem)] p-0.5 md:p-1 flex items-center justify-center rounded-md uppercase text-[min(1.4vw,0.75rem)] inset-shadow-xs inset-shadow-black";
-
-  const baseStats: string[] = [
-    "Hp",
-    "Attack",
-    "Defense",
-    "Special Attack",
-    "Special Defense",
-    "Speed",
-  ];
 
   const fetchPokemon = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -49,11 +40,11 @@ const App = () => {
   };
 
   return (
-    <div className="bg-dark-blue font-display h-full flex flex-col items-center bg-game-2 bg-fixed">
+    <div className="bg-game-2 bg-fixed font-display h-screen w-full sm:h-full flex flex-col items-center ">
       <div
         onMouseEnter={handleMouseEnterLeave}
         onMouseLeave={handleMouseEnterLeave}
-        className="fixed top-0 right-0 h-48 hover:h-52 hover:cursor-pointer hover:drop-shadow-2xl hover:drop-shadow-indigo-600 text-[0.8rem]"
+        className="fixed top-0 right-0 h-[min(15vh,12rem)] hover:h-52 hover:cursor-pointer hover:drop-shadow-2xl hover:drop-shadow-indigo-600 text-[0.8rem]"
       >
         <img src={pokemonCards} alt="Memory Game" className="z-10 h-full" />
 
@@ -148,7 +139,7 @@ const App = () => {
                 type="submit"
                 id="search-button"
                 className="absolute top-[6%] left-[3%] h-[26%] w-[23%] rounded-full text-white border-0 border-emerald-500 hover:cursor-pointer"
-              ><IoMdSearch className="h-[70%] w-full hover:h-[63%]" /></button>
+              ><IoMdSearch className="h-[67%] w-full hover:h-[60%]" /></button>
             </form>
           </div>
 
@@ -162,8 +153,9 @@ const App = () => {
             className="max-h-[750px] border-0 border-amber-300 object-contain"
           />
 
-          <div className="absolute top-[21.5%] left-[8%] h-[68%] w-[75%] bg-green-200 border-4 md:border-6 border-gray-900 rounded-3xl ">
-            <div id="size" className="text-[min(1.5vw,0.9rem)] border-0 border-pink-600">
+          <div className="absolute top-[21.5%] left-[8%] h-[68%] w-[75%] bg-emerald-400 border-2 border-gray-900 rounded-2xl p-3 flex flex-col justify-around items-center inset-shadow-sm inset-shadow-black ">
+
+            <div id="size" className="text-[min(2vw,1rem)] border-0 border-pink-600">
               <span id="weight">
                 {pokemon?.weight && "Weight: " + pokemon?.weight}
               </span>{" "}
@@ -171,101 +163,19 @@ const App = () => {
                 {pokemon?.height && "Height: " + pokemon?.height}
               </span>
             </div>
-          </div>
 
-        </div>
-      </div>
-
-      <div className="z-20 bg-white w-[450px] my-8 px-4 py-5 rounded-2xl shadow-[10px_10px_grey] flex flex-col justify-center items-center gap-2">
-        <form id="search-form" className="mb-3 mt-1" onSubmit={fetchPokemon}>
-          <label htmlFor="search-input" className="block mb-3">
-            Search for a Pokémon Name or ID:
-          </label>
-          <input
-            id="search-input"
-            name="search-input"
-            type="text"
-            value={nameOrId}
-            onChange={(e) => setNameOrId(e.target.value)}
-            className="p-3 mr-3 outline-1 rounded-md focus:outline-3 focus:outline-blue-700 focus:rounded-md"
-            required
-          />
-          <button
-            type="submit"
-            id="search-button"
-            className="w-25 py-4 rounded-full bg-yellow-500 text-white text-sm hover:cursor-pointer"
-          >
-            Search
-          </button>
-        </form>
-
-        <div
-          id="output"
-          className="w-full min-h-[325px] p-3 flex flex-col justify-center"
-        >
-          <div id="name-and-id" className="h-7 mb-1 text-lg">
-            <span id="pokemon-name" className="uppercase">
-              {pokemon?.name}
-            </span>{" "}
-            <span id="pokemon-id">{pokemon?.id && "#" + pokemon?.id}</span>
-          </div>
-
-          <div id="size" className="text-sm">
-            <span id="weight">
-              {pokemon?.weight && "Weight: " + pokemon?.weight}
-            </span>{" "}
-            <span id="height">
-              {pokemon?.height && "Height: " + pokemon?.height}
-            </span>
-          </div>
-
-          <div
-            id="sprite-container"
-            className="flex items-center justify-center grow-2"
-          >
-            {pokemon?.sprites && (
-              <img
-                id="sprite"
-                src={
-                  pokemon?.sprites.versions["generation-v"]["black-white"]
-                    .animated.front_default || pokemon?.sprites.front_default
-                }
-                alt={pokemon?.name + " sprite"}
-                className={`${pokemon?.id >= 650 ? "w-52" : "w-32"}`}
-              />
-            )}
-          </div>
-
-          <div
-            id="types"
-            className="min-h-8 flex flex-wrap justify-start gap-2"
-          >
-            {pokemon?.types.map((pokeType, index) => (
-              <span
-                key={index}
-                id={"type-" + pokeType.type.name}
-                className={`${baseTypeClass} ${
-                  typeColors[pokeType.type.name] || "bg-red-400"
-                }`}
-              >
-                {pokeType.type.name}
-              </span>
-            ))}
-          </div>
-        </div>
-
-        <div id="base-stats-div" className="w-full max-w-[450px]">
-          <table className="w-full bg-indigo-800 text-white text-center">
-            <tbody>
-              <tr>
-                <th className={tableDataStyle}>Base</th>
-                <th className={tableDataStyle}>Stats</th>
-              </tr>
-              {pokemon?.stats
-                ? pokemon?.stats.map((pokeStat) => (
-                    <tr>
-                      <td className={`${tableDataStyle} w-3/5 capitalize`}>
-                        {pokeStat.stat.name.replace(/-/g, " ")}:
+            { pokemon?.stats &&
+              <table className="w-full text-center text-gray-900">
+                <tbody>
+                  <tr>
+                    <th className={tableDataStyle}>BASE</th>
+                    <th className={tableDataStyle}>STATS</th>
+                  </tr>
+              
+                { pokemon?.stats.map((pokeStat, index) => (
+                    <tr key={index}>
+                      <td className={`${tableDataStyle} w-3/5 ${pokeStat.stat.name === "hp" ? "uppercase" : "capitalize"}`}>
+                        {pokeStat.stat.name.replace(/-/g, " ")}
                       </td>
                       <td
                         className={`${tableDataStyle} w-2/5`}
@@ -275,14 +185,11 @@ const App = () => {
                       </td>
                     </tr>
                   ))
-                : baseStats.map((base) => (
-                    <tr>
-                      <td className={`${tableDataStyle} w-3/5`}>{base}:</td>
-                      <td className={`${tableDataStyle} w-2/5`}></td>
-                    </tr>
-                  ))}
-            </tbody>
-          </table>
+                }
+                </tbody>
+              </table>
+            }
+          </div>
         </div>
       </div>
     </div>
