34c0c9d589e6216cdb31623a120c867e4d6a5aa5
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-07-16 00:43:05 +0500

    Passing props to Hero component from HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,12 +2,13 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Pokedex from "../components/Pokedex";
+import logo2 from "../assets/images/logo2.png";
 
 const HomePage = () => {
   return (
     <div className="bg-game-2 bg-fixed font-display h-screen w-full sm:h-full flex flex-col items-center ">
       <Navbar />
-      <Hero />
+      <Hero logo={logo2} title="Pokémon Search App" cssHeight="h-32" hasShadow={true} />
       <Pokedex />
     </div>
   );
