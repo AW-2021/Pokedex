6ff27e35f345daa8c4e1d13ee34a3114837f220f
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-07-16 00:39:48 +0500

    Modified Hero.tsx to take in props now

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,27 @@
-import logo4 from "../assets/images/logo4.png";
 import pokeball2 from "../assets/images/pokeball2.webp";
 
-const Hero = () => {
+interface HeroProps {
+  logo: string;
+  title: string;
+  cssHeight: string;
+  hasShadow: boolean;
+}
+
+const Hero = ({ logo, title, cssHeight, hasShadow }: HeroProps) => {
   return (
     <>
       <img
-        src={logo4}
-        className="h-32 w-auto mt-6 mb-4 z-20 drop-shadow-lg drop-shadow-amber-400"
+        src={logo}
+        alt="logo"
+        className={cssHeight +` w-auto mt-6 mb-4 z-20 drop-shadow-lg drop-shadow-amber-400`}
       />
-      <div className="z-20">
+      <div className="z-20 mb-2">
         <img
           src={pokeball2}
           className="h-9 w-auto inline align-top animate-bounce"
         />
-        <span className="font-bold text-3xl text-shadow-sm text-shadow-white">
-          {" "}
-          Pokémon Search App{" "}
+        <span className={`font-bold text-3xl ${hasShadow && "text-shadow-sm text-shadow-white"}`}>
+          {" "}{title}{" "}
         </span>
         <img
           src={pokeball2}
