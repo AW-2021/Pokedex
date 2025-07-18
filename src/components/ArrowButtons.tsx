65da6d6b24df4cef5a03446138f32af3a35f0151
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-07-18 23:29:53 +0500

    Moved React + TypeScript + TailwindCSS project into a separate frontend folder

--- src/components/ArrowButtons.tsx
+++
@@ -1,8 +0,0 @@
-
-const ArrowButtons = () => {
-  return (
-    <div className="absolute right-[18%] bottom-[19%] h-[15%] w-[21%] border-1 border-cyan-300"></div>
-  )
-}
-
-export default ArrowButtons
