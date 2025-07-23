a69fd645b832731844e36aa22ee6c545f81dfc87
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-07-24 00:37:04 +0500

    Updated Card.tsx

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,11 +1,20 @@
+import { type CardType } from "../types";
 interface CardProps {
-    num: number;
+  card: CardType;
 }
 
-const Card = ({ num }: CardProps) => {
+const Card = ({ card }: CardProps) => {
+
+  const handleClick = () => {
+    console.log("HELLO!");
+  }
+
   return (
-    <div className="border-1 border-white">Card {num}</div>
-  )
-}
+    <div onClick={handleClick} className="bg-medium-blue p-4 rounded-lg hover:shadow-amber-400 hover:shadow-lg">
+      <img src={card.images.small} alt={card.name} className="mx-auto mb-4" />
+      <h2 className="text-xl font-semibold text-center">{card.name}</h2>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
