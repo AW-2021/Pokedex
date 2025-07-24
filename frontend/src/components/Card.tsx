188130cd5ac7186dcbcee5a9f18b24b4df70d213
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-07-24 23:45:06 +0500

    Modified box-shadow in Card.tsx

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -7,9 +7,9 @@ interface CardProps {
 const Card = ({ card, handleClick }: CardProps) => {
   
   return (
-    <div onClick={() => handleClick(card.id)} className="bg-medium-blue p-4 rounded-lg cursor-pointer hover:shadow-amber-400 hover:shadow-lg">
+    <div onClick={() => handleClick(card.id)} className="bg-medium-blue p-4 rounded-lg cursor-pointer hover:shadow-amber-400 hover:shadow-[0_0_10px_7px]">
       <img src={card.images.small} alt={card.name} className="mx-auto mb-4" />
-      <h2 className="text-xl font-semibold text-center">{card.name}</h2>
+      <h2 className="text-xl font-semibold text-center ">{card.name}</h2>
     </div>
   );
 };
