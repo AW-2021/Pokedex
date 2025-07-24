77299fba448527fe33c740be2f1511b69bf7715b
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-07-24 23:46:46 +0500

    Fixed event handler function for onClick event and updated nav icon to resize and change with window

--- frontend/src/pages/CardGamePage.tsx
+++ frontend/src/pages/CardGamePage.tsx
@@ -1,13 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ogLogo from "../assets/images/og-logo.png";
 import pokedexIcon from "../assets/images/pokedexIcon.png";
 import pikachu from "../assets/images/runningpika.webp";
 import Card from "../components/Card";
 import Hero from "../components/Hero";
-import { type CardType } from "../types"
+import useScreenSize from "../hooks/useScreenSize";
+import { IoIosBook } from "react-icons/io";
+import { type CardType } from "../types";
+
+const ARR_LENGTH: number = 24;
 
 const CardGamePage = () => {
+  const screenSize: string = useScreenSize();
   const [cards, setCards] = useState<CardType[] | null>(null);
   const [chosenCards, setChosenCards] = useState<string[]>([]);
   const [score, setScore] = useState<number>(0);
@@ -17,16 +22,18 @@ const CardGamePage = () => {
   useEffect(() => {
     const fetchCards = async () => {
       try {
-        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/cards`);
+        const res = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/api/cards`
+        );
         // const res = await fetch(`http://localhost:8000/api/cards`);
         const data = await res.json();
-        setCards(data?.data.slice(0,24));
+        setCards(data?.data?.slice(0, ARR_LENGTH));
       } catch (err) {
         console.log("Error", err);
       } finally {
         setLoading(false);
       }
-    }
+    };
 
     fetchCards();
   }, []);
@@ -35,12 +42,16 @@ const CardGamePage = () => {
     console.log(id);
 
     if (chosenCards.includes(id)) {
-      setHighScore(score);
+      // Reset game
+      score > highScore && setHighScore(score);
       setScore(0);
+      setChosenCards([]);
     } else {
       setScore((prevScore) => prevScore + 1);
       setChosenCards((prevCards) => [...prevCards, id]);
     }
+
+    console.log(chosenCards);
 
     const cardsSet: Set<CardType> = new Set();
 
@@ -48,52 +59,71 @@ const CardGamePage = () => {
       while (cardsSet.size < cards?.length) {
         const index: number = Math.floor(Math.random() * cards?.length);
         cardsSet.add(cards[index]);
-        setCards(Array.from(cardsSet))
       }
+      setCards(Array.from(cardsSet));
     }
     console.log(cardsSet);
-  }
+  };
 
   return (
     <div className="w-full h-full min-h-screen flex flex-col items-center bg-dark-blue font-display text-white">
-      <Link to="/" className="absolute top-4 left-5 h-[min(18vw,11rem)] -rotate-6">
-        <img src={pokedexIcon} alt="pokedex icon" className="z-10 h-full hover:drop-shadow-blue-400 hover:drop-shadow-xl" />
+      <Link
+        to="/"
+        className="absolute top-4 left-3 h-[min(17vw,11rem)] hover:drop-shadow-blue-400 hover:drop-shadow-xl text-"
+      >
+        { screenSize === "sm" ? (
+          <IoIosBook className="h-[58%] w-full p-2 border-4 border-blue-600 text-blue-600 bg-yellow-300 inset-ring-4 inset-ring-yellow-500 rounded-full" />
+        ) : (
+          <img
+            src={pokedexIcon}
+            alt="pokedex icon"
+            className="z-10 h-full"
+          />
+        )}
       </Link>
 
       <Hero logo={ogLogo} title="Memory Card Game" cssStyle="h-34 mb-6" hasShadow={false} />
-      
-      {
-        loading ? (
-          <div className="my-auto mx-auto flex flex-col items-center gap-10">
-            <img src={pikachu} alt="Loading..." className="max-w-[20rem]" />
-            <p className='text-lg font-semibold tracking-widest text-shadow-lg text-shadow-black '>LOADING ....</p>
-          </div>
-        ) : (
-          <div className="w-full py-4 px-10 border-0 border-yellow-300 my-6 flex flex-col justify-center items-center gap-8">
-            <div className="w-full border-0 border-purple-600 flex justify-between items-center">
-              <div className="border-0 border-red-600">
-                <p>Get points by clicking on an image</p> 
-                <p>but don't click on any more than once!</p>
-              </div>
-              <div className="border-0 border-cyan-400">
-                <p>Score: {score}</p>
-                <p>High Score: {highScore}</p>
-              </div>
-            </div>
 
-            {/* <div className="border-2 border-emerald-600 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] auto-rows-[minmax(400px,2fr)] grid-flow-row gap-10"> */}
-            <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
-              {
-                cards?.map(card => (
-                  <Card key={card.id} card={card} handleClick={handleClick} />
-                ))
-              }
+      {loading ? (
+        <div className="my-auto mx-auto flex flex-col items-center gap-10">
+          <img src={pikachu} alt="Loading..." className="max-w-[20rem]" />
+          <p className="text-lg font-semibold tracking-widest text-shadow-lg text-shadow-black ">
+            LOADING ....
+          </p>
+        </div>
+      ) : (
+        <div className="w-full py-4 px-16 border-0 border-yellow-300 my-6 flex flex-col justify-center items-center gap-8">
+          <div className="w-full border-0 border-purple-600 flex justify-between items-center">
+            <div className="border-0 border-red-600">
+              <p className="text-shadow-lg text-shadow-black">
+                Get points by clicking on an image
+              </p>
+              <p className="text-shadow-lg text-shadow-black">
+                but don't click on any more than once!
+              </p>
+            </div>
+            <div className="border-0 border-cyan-400">
+              <p className="text-shadow-lg text-shadow-black">
+                <span className="text-shadow-none">🎲</span> Score:{" "}
+                <span className="font-bold">{score}</span>
+              </p>
+              <p className="text-shadow-lg text-shadow-black">
+                <span className="text-shadow-none">⭐️ </span> High Score:{" "}
+                <span className="font-bold">{highScore}</span>
+              </p>
             </div>
           </div>
-        )
-      }
+
+          {/* <div className="border-2 border-emerald-600 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] auto-rows-[minmax(400px,2fr)] grid-flow-row gap-10"> */}
+          <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+            {cards?.map((card) => (
+              <Card key={card.id} card={card} handleClick={handleClick} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default CardGamePage;
