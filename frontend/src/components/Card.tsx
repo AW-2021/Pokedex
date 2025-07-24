06a752a5fc6b401c11c05f2888882534e68a5c10
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-07-24 20:05:07 +0500

    Added an onClick event to the Card component

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,16 +1,13 @@
 import { type CardType } from "../types";
 interface CardProps {
   card: CardType;
+  handleClick: (id: string) => void;
 }
 
-const Card = ({ card }: CardProps) => {
-
-  const handleClick = () => {
-    console.log("HELLO!");
-  }
-
+const Card = ({ card, handleClick }: CardProps) => {
+  
   return (
-    <div onClick={handleClick} className="bg-medium-blue p-4 rounded-lg hover:shadow-amber-400 hover:shadow-lg">
+    <div onClick={() => handleClick(card.id)} className="bg-medium-blue p-4 rounded-lg cursor-pointer hover:shadow-amber-400 hover:shadow-lg">
       <img src={card.images.small} alt={card.name} className="mx-auto mb-4" />
       <h2 className="text-xl font-semibold text-center">{card.name}</h2>
     </div>
