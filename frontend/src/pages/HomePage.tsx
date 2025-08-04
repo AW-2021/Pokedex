a0d4cf9292023eb5a3c06ce81b17f2f64847e88d
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-08-04 22:53:18 +0500

    Modified HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ import logo2 from "../assets/images/logo2.png";
 
 const HomePage = () => {
   return (
-    <div className="bg-game-2 bg-fixed font-display h-screen w-full sm:h-full flex flex-col items-center ">
+    <div className="bg-game-2 bg-fixed xl:bg-cover font-display h-screen w-full sm:h-full flex flex-col items-center ">
       <HomeNavbar />
       <Hero logo={logo2} title="Pokémon Search App" cssStyle="h-32 mb-4" hasShadow={true} />
       <Pokedex />
