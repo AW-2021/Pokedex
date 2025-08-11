340120f422becaa71a78e228578dde64a2186c60
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-08-11 16:07:22 +0500

    Modified Loader.tsx

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -2,10 +2,10 @@ import pikachu from "../assets/images/runningpika.webp";
 
 const Loader = () => {
   return (
-    <div className="my-auto mx-auto flex flex-col items-center gap-10">
+    <div className="my-auto flex flex-col items-center justify-center gap-10 mt-10">
       <img src={pikachu} alt="Loading..." className="max-w-[20rem]" />
-      <p className="text-lg font-semibold tracking-widest text-shadow-lg text-shadow-black ">
-        LOADING ....
+      <p className="text-lg font-semibold uppercase text-shadow-lg text-shadow-black">
+        CATCHING THEM ALL ....
       </p>
     </div>
   );
